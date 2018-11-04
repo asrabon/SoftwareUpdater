7acbfaeafd3963fccec729c421d36d3662e8e2cf
Sloan Rabon <sloanrabon@gmail.com>
2018-11-04 12:10:33 -0500

    Almost finished with NPP 11-4-18

--- updater.py
+++ updater.py
@@ -4,17 +4,21 @@ import os
 import subprocess
 
 from bs4 import BeautifulSoup
+import wget
 import wmi
 
 s = session()
 w = wmi.WMI()
 
 SOFTWARE = {'Notepad++ (32-bit x86)': {'download_page': 'https://notepad-plus-plus.org/download/',
+                                       'download_location': lambda page: page.find(id='main').find_all('div')[3].find('a')['href'],
+                                       'install': lambda installer_location: subprocess.Popen('{} /S'.format(installer_location), stdout=subprocess.PIPE),
                                        'version_location': lambda page: page.find('h1').text.split(' ')[-1].strip(),
                                        'website_root': 'https://notepad-plus-plus.org'}
             }
 
 GET_PROGRAMS_POWERSHELL = os.path.join('./', 'Get-Programs.ps1')
+UPDATE_FOLDER = os.path.join('./', 'updates')
 
 def main():
     programs = get_programs()
@@ -34,7 +38,6 @@ def get_programs():
     out = out.decode('utf-8').split('\n')[1:]
     for line in out:
         try:
-            print(line)
             name, version, publisher, uninstall_command, install_date = line.split('*')
             programs[name] = {'version': version,
                             'publisher': publisher,
@@ -49,14 +52,21 @@ def get_latest_version(name, bit):
     link = SOFTWARE[name]['download_page']
     download_page = s.get(link, timeout=10).text
     soup = BeautifulSoup(download_page, 'html.parser')
-    #version = soup.find('h1').text.split(' ')[-1].strip()
     version = SOFTWARE[name]['version_location'](soup)
-    download_link = soup.find(id='main').find_all('div')[3].find('a')['href']
+    download_link = SOFTWARE[name]['download_location'](soup)
     web_root = SOFTWARE[name]['website_root']
     if web_root not in download_link:
         download_link = web_root + download_link
-    #download_link = soup.select('#main > div:nth-child(8) > ul:nth-child(2) > li:nth-child(1) > strong:nth-child(1) > a:nth-child(1)')['href']
     print(download_link)
+    installer_path = wget.download(download_link, UPDATE_FOLDER)
+    rt = SOFTWARE[name]['install'](installer_path)
+    print(rt)
+    '''
+    r = w.Win32_Product.Install (
+        PackageLocation= installer_path,
+        AllUsers=False
+    )'''
+    #print(r)
     return version
 
 
